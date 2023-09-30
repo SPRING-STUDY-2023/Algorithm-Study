7c7ec17dd59d4a255b2bef510a5873d937879410
yujindonut <bbb3631@naver.com>
2023-09-30 22:06:32 +0900

    [WEEK3] Silver1_링크와스타트

---
+++ yujin/Week3/Silver1_링크와스타트.java
@@ -0,0 +1,28 @@
+package Week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Silver1_링크와스타트 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] map = new int[n][n];
+        StringTokenizer st;
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+
+            }
+        }
+    }
+}
