7da356b170d5462dd5ca7a31dc9c7591c88f5a20
yujindonut <bbb3631@naver.com>
2023-10-06 09:04:06 +0900

    [WEEK4] 수찾기

--- yujin/Week4/SIlver4_수찾기.java
+++ yujin/Week4/SIlver4_수찾기.java
@@ -28,19 +28,17 @@ public class SIlver4_수찾기 {
                 System.out.println(0);
             }
         }
-
     }
     public static boolean binarySearch(int n, int[] arr) {
         int l = 0, r = arr.length - 1;
-
-        while(l < r) {
-            int mid = l + r / 2;
+        while(l <= r) {
+            int mid = (l + r) / 2;
             if(arr[mid] == n) {
                 return true;
             } else if(arr[mid] > n) {
-                l = mid + 1;
-            } else {
                 r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
         return false;
