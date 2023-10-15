476587e5b74743339a66800cf6126bf673db53d3
thguss <thgus345@naver.com>
2023-10-15 16:51:32 +0900

    [FIX] 클래스명 변경

--- sohyeon/week4/K번째수_1300.java
+++ sohyeon/week4/K번째수_1300.java
@@ -1,7 +1,10 @@
-import java.util.*;
-import java.io.*;
+package week4;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class K번째수_1300 {
 
 	/**
 	 * 놓친 점
