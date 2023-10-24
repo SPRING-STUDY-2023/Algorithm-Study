e81966181cb6f1c11fad3ac5fb1be44047563aa7
thguss <thgus345@naver.com>
2023-10-24 17:04:00 +0900

    [Week6] 1. 1,2,3 더하기

--- sohyeon/week6/_123더하기_9095.java
+++ sohyeon/week6/_123더하기_9095.java
@@ -1,7 +1,10 @@
-import java.util.*;
-import java.io.*;
+package week6;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _123더하기_9095 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
