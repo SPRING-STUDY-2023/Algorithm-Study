8b260c86b5d37f6364dd62ce9a9a2db69118c901
thguss <thgus345@naver.com>
2023-10-24 17:52:05 +0900

    [Week6] 2. 2xn 타일링

---
+++ sohyeon/week6/_2n타일링_11726.java
@@ -0,0 +1,31 @@
+package week6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2n타일링_11726 {
+
+	static final int MOD = 10007;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+
+		if (n == 1) {
+			System.out.println(1);
+		} else if (n == 2) {
+			System.out.println(2);
+		} else {
+			int[] dp = new int[n + 1];
+			dp[1] = 1;
+			dp[2] = 2;
+			for (int i = 3; i <= n; i++) {
+				dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+			}
+			System.out.println(dp[n]);
+		}
+	}
+}
