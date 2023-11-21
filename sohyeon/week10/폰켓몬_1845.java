81b64407e8ebae8cced44900a92c384cdb5c9fc1
thguss <thgus345@naver.com>
2023-11-21 20:55:16 +0900

    [Week10] 3. 폰켓몬

---
+++ sohyeon/week10/폰켓몬_1845.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+	public int solution(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int num : nums) {
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+
+		return map.size() >= (nums.length / 2) ? (nums.length / 2) : map.size();
+	}
+}
