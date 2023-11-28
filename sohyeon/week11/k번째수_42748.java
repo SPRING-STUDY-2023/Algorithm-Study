dad7b18f458f895144037643284f882db6c5af2d
thguss <thgus345@naver.com>
2023-11-28 15:23:03 +0900

    [Week11] 3. k번째 수

---
+++ sohyeon/week11/k번째수_42748.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+
+		for (int i = 0; i < commands.length; i++) {
+			int s = commands[i][0];
+			int e = commands[i][1];
+			int k = commands[i][2];
+			answer[i] = find(array, s, e, k);
+		}
+
+		return answer;
+	}
+
+	private int find(int[] arr, int s, int e, int k) {
+		List<Integer> al = new ArrayList<>();
+		for (int i = s - 1; i <= e - 1; i++) {
+			al.add(arr[i]);
+		}
+		Collections.sort(al);
+		return al.get(k - 1);
+	}
+}
