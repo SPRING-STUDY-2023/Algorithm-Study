b1b075e1f48d6d8781e0ac134cf90f756647514f
yeseul106 <20191037@sungshin.ac.kr>
2023-11-30 19:25:06 +0900

    [WEEK11] 프로그래머스 K진수에서 소수 개수 구하기 92335

---
+++ yeseul/WEEK11/src/프로그래머스_K진수에서소수개수구하기_92335.java
@@ -0,0 +1,35 @@
+public class 프로그래머스_K진수에서소수개수구하기_92335 {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String convertedNum = convertToBaseK(n, k);
+
+        int j = 0;
+        for (int i = 0; i < convertedNum.length() - 1; i = j) {
+            for (j = i + 1; j < convertedNum.length() && convertedNum.charAt(j) != '0'; j++) ;
+            if (isPrime(Long.parseLong(convertedNum.substring(i, j))))
+                answer++;
+        }
+        return answer;
+    }
+
+    private static boolean isPrime(long num) {
+        if (num <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    private static String convertToBaseK(int n, int k) {
+        StringBuilder str = new StringBuilder();
+
+        while (n > 0) {
+            str.append(n % k);
+            n /= k;
+        }
+
+        str = str.reverse();
+        return str.toString();
+    }
+}
