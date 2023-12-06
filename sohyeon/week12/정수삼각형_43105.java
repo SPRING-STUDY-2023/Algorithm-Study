dc3e8fbe785eae5fb45eeb5704bb72725235321b
thguss <thgus345@naver.com>
2023-12-06 16:13:52 +0900

    [Week12] 3. 정수 삼각형

---
+++ sohyeon/week12/정수삼각형_43105.java
@@ -0,0 +1,21 @@
+package week12;
+
+public class 정수삼각형_43105 {
+	public int solution(int[][] triangle) {
+		int n = triangle.length;
+		int m = triangle[triangle.length - 1].length;
+		int[][] dp = new int[n][m];
+
+		for (int i = 0; i < m; i++) {
+			dp[n - 1][i] = triangle[n - 1][i];
+		}
+
+		for (int i = n - 2; i >= 0; i--) {
+			for (int j = 0; j < triangle[i].length; j++) {
+				dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+			}
+		}
+
+		return dp[0][0];
+	}
+}
